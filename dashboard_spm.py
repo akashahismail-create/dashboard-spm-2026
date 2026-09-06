52ecad2d82181d897fb52f7b8e44bebc336d60c0
akashahismail-create <akashah.ismail@moe.gov.my>
2026-09-07 02:26:46 +0800

    Update dashboard_spm.py

--- dashboard_spm.py
+++ dashboard_spm.py
@@ -24,22 +24,6 @@ def load_data():
 df_jadual, df_nama, df_pusat, df_bilik, df_full = load_data()
 st.markdown("---") # Garisan pemisah
 
-# BUTTON PENGURUSAN CALON
-st.markdown("---")
-
-# BUAT 2 KOLUM: Kiri kosong, Kanan butang
-col_kosong, col_butang = st.columns([4, 1])
-
-with col_butang:
-    st.markdown("""
-    <a href="https://script.google.com/macros/s/AKfycbwav3jbWQEkTW2yTK9PnanlItxPM5NpCHADLNb_BRjY4hmsale257tSqMsRTdqv88HA/exec" 
-       target="_blank" 
-       style="display:block; padding:10px; font-size:14px; font-weight:bold; color:white; 
-              background:#2563eb; text-align:center; text-decoration:none; border-radius:8px;">
-       📝 CALON
-    </a>
-    """, unsafe_allow_html=True)
-
 st.markdown("---") 
 
 st.markdown("---")
@@ -73,6 +57,22 @@ col1, col2, col3 = st.columns(3)
 with col1: st.metric("Jumlah Pusat", df_nama['NO PUSAT'].nunique())
 with col2: st.metric("Jumlah Bilik Kebal", len(df_bilik))
 with col3: st.metric("Jumlah Subjek", df_jadual['KOD MATA PELAJARAN'].nunique())
+    
+# BUTTON PENGURUSAN CALON
+st.markdown("---")
+
+# BUAT 2 KOLUM: Kiri kosong, Kanan butang
+col_kosong, col_butang = st.columns([4, 1])
+
+with col_butang:
+    st.markdown("""
+    <a href="https://script.google.com/macros/s/AKfycbwav3jbWQEkTW2yTK9PnanlItxPM5NpCHADLNb_BRjY4hmsale257tSqMsRTdqv88HA/exec" 
+       target="_blank" 
+       style="display:block; padding:10px; font-size:14px; font-weight:bold; color:white; 
+              background:#2563eb; text-align:center; text-decoration:none; border-radius:8px;">
+       📝 CALON
+    </a>
+    """, unsafe_allow_html=True)
 
 st.write("---")
 
