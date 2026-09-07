21173ca38a7d4cde1c2a4571f33301283c509df6
akashahismail-create <akashah.ismail@moe.gov.my>
2026-09-08 01:16:31 +0800

    Update dashboard_spm.py

--- dashboard_spm.py
+++ dashboard_spm.py
@@ -102,7 +102,11 @@ if st.session_state.get("show_pw", False):
 st.write("---")
 
 tab1, tab2, tab3, tab4, tab5 = st.tabs([
-    "🏫 Senarai Pusat", "🔒 Senarai Bilik Kebal", "📅 Jadual", "🔍 Carian", "📈 Analisis"
+    "🏫 Senarai Pusat", 
+    "🔒 Senarai Bilik Kebal", 
+    "📅 Jadual", 
+    "🔍 Carian", 
+    "📈 Analisis"
 ])
 
 with tab1:
